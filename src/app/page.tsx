cac18e6a3794a82e4c22e6d3acb34ca61802df24
A Galliher <LiveTimeData@gmail.com>
2026-03-04 15:00:06 -0500

    Fix homepage to use dashboard UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 // src/app/page.tsx
 
-import CityDashboardClient from "./city-dashboard/CityDashboardClient";
+import CityDashboardClient from "./city-dashboard/ui";
 
 export default function HomePage() {
-  return (
-    <CityDashboardClient
-      initialQuery=""
-      autoPickFirst={false}
-    />
-  );
+  return <CityDashboardClient initialQuery="" />;
 }
