6926f54b081b1840bbafb43aabee4f08e75e8484
A Galliher <LiveTimeData@gmail.com>
2026-03-09 16:00:32 -0400

    Add raw AdSense script to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
-
 import Footer from "@/components/Footer";
 
 const geistSans = Geist({
@@ -28,13 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
-          id="adsbygoogle-init"
+        <script
           async
-          strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8921617153359907"
           crossOrigin="anonymous"
-        />
+        ></script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
